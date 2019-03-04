b5bfd1c8684a388b85822ab3219244cabf6773a5
k86095 <k86095@gmail.com>
2019-03-04 16:16:42 +0800

    Initial Commit

---
+++ AdministratorModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdministratorModel
+//
+//  Created by 方錦泉 on 2019/3/4.
+//  Copyright © 2019 方錦泉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
